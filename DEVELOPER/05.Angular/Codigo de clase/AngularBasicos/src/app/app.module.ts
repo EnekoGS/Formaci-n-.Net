4fc73b3f55e433021fd1fd2ea4792770cfaa0d15
EnekoGS <enekogalvan@gmail.com>
2024-06-18 14:14:21 +0200

    cambios angular martes

--- DEVELOPER/05.Angular/Codigo de clase/AngularBasicos/src/app/app.module.ts
+++ DEVELOPER/05.Angular/Codigo de clase/AngularBasicos/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BindingComponent } from './introduccion/binding/binding.component';
 import { FormsModule } from '@angular/forms';
+import { DirectivasComponent } from './introduccion/directivas/directivas.component';
+import { ActividadNgForComponent } from './introduccion/actividad-ng-for/actividad-ng-for.component';
+import { AmigosComponent } from './introduccion/amigos/amigos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BindingComponent
+    BindingComponent,
+    DirectivasComponent,
+    ActividadNgForComponent,
+    AmigosComponent
   ],
   imports: [
     BrowserModule,
---
+++ DEVELOPER/05.Angular/Codigo de clase/AngularBasicos/src/app/interfaces/alumno.interface.ts
@@ -0,0 +1,7 @@
+export interface IAlumno {
+    nombre: string;
+    asignatura: string;
+    nota: number;
+}
+
+//-------------------------------------------------------------
