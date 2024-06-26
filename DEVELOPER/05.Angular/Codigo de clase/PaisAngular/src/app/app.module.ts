641e3e7e4d28c25e413c0cbdd37d6ec8dff56507
EnekoGS <enekogalvan@gmail.com>
2024-06-26 10:15:32 +0200

    actividad angular

--- DEVELOPER/05.Angular/Codigo de clase/PaisAngular/src/app/app.module.ts
+++ DEVELOPER/05.Angular/Codigo de clase/PaisAngular/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginasModule } from './paginas/paginas.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PaginasModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
