646a8e5c50dfd3b50a58225d8438c0de2be321d8
EnekoGS <enekogalvan@gmail.com>
2024-06-26 13:11:30 +0200

    finalizado

--- DEVELOPER/05.Angular/Codigo de clase/PaisAngular/src/app/paginas/paginas.module.ts
+++ DEVELOPER/05.Angular/Codigo de clase/PaisAngular/src/app/paginas/paginas.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { PaisesComponent } from './paises/paises.component';
 import { Error404Component } from './error404/error404.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -15,7 +16,8 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FormsModule
   ],
   exports: [
     HomeComponent,
--- DEVELOPER/05.Angular/Codigo de clase/PaisAngular/src/app/paginas/paises/paises.component.ts
+++ DEVELOPER/05.Angular/Codigo de clase/PaisAngular/src/app/paginas/paises/paises.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IPaises } from './paises.interface';
+import { PaisesService } from './paises.service';
 
 @Component({
   selector: 'app-paises',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./paises.component.css']
 })
 export class PaisesComponent {
+  monedaSeleccionada: string = '';
+  monedaPais: IPaises[] = [];
 
+  mostrarError = false;
+
+  constructor(private paisesService: PaisesService) {}
+
+  getMoneda() {
+    this.paisesService.getMoneda(this.monedaSeleccionada).subscribe({
+      next: (data: IPaises[]) => {
+        console.log(data);
+        this.monedaPais = data;
+
+        this.mostrarError = false;
+      },
+      error: (err) => (this.mostrarError = true),
+      complete: () => console.log('ok')
+    })
+  }
 }
